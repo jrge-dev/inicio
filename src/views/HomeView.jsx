3bc12f8551752622b9ac5e8e25351a93b7c1049b
Jrge <jorgemoymoya@gmail.com>
2024-07-21 00:35:25 -0400

    Create and add HomeView

---
+++ src/views/HomeView.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header"
+import Home from "../layouts/Home"
+
+function HomeView() {
+    
+    return(
+        <>
+        <Header/>
+        <Home/>
+        </>
+    )
+}
+
+export default HomeView
