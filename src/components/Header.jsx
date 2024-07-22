a3d169f9033a55e8ddf74d13b5a1c47ae0811c49
Jrge <jorgemoymoya@gmail.com>
2024-07-22 00:36:56 -0400

    New component header with the responsive mobile disign

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import icon from "../assets/bug.svg";
 
 function Header() {
+  const [isMenuOpen, setIsMenuIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuIsOpen(!isMenuOpen);
+  };
   return (
     <>
-      <header className="flex justify-between font-bold text-lg p-4">
-        <div className="flex space-x-6 h-6 font-bold">
-          <img src={icon} alt="" /> <h3>Jorge Moya</h3>
+      <header className="font-bold p-4 flex flex-col sm:flex-row sm:justify-between sm:text-lg">
+        <div className="flex mb-10 sm:mb-0  space-x-6 h-6 font-bold">
+          <img className="w-5 h-5" src={icon} alt="" /> <h3>Jorge Moya</h3>
         </div>
 
-        <nav className="space-x-12">
+        <button className="absolute right-4  sm:hidden" onClick={toggleMenu}>
+          {isMenuOpen ? <p>abrir</p> : <p>cerrar</p>}
+        </button>
+
+        <nav
+          className={`flex flex-col sm:flex-row ${isMenuOpen ? 'block' : 'hidden'} sm:block items-center space-y-4 sm:space-y-0  sm:space-x-12`}
+        >
           <a href="">Inicio</a>
           <a href="">Sobre mi</a>
           <a href="">Tecnologías</a>
