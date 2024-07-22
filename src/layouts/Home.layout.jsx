4bcde6eb822a960030652429fa884b85329bc9e6
Jrge <jorgemoymoya@gmail.com>
2024-07-22 12:02:21 -0400

    add mobile version for the home.layout

--- src/layouts/Home.layout.jsx
+++ src/layouts/Home.layout.jsx
@@ -2,12 +2,12 @@ import imageDeveloper from "../assets/image-developer.svg";
 function Home() {
   return (
     <>
-      <div className="flex justify-between items-center px-20 w-full max-w-screen">
+      <div className="flex flex-col-reverse  sm:flex-row justify-between items-center px-20 w-full max-w-screen">
         <div className="w-full h-full">
           <img src={imageDeveloper} alt="image developer" />
         </div>
-        <div className="w-full h-full text-4xl ">
-          <p className="text-blue-500 font-bold text-5xl mb-4 ">¡Hola!</p>
+        <div className="w-full h-full text-xl sm:text-xl">
+          <p className="text-blue-500 font-bold text-4xl sm:text-5xl mb-4 ">¡Hola!</p>
           <p className="mb-2">
             Mi nombre es <b>Jorge Moya</b>
           </p>
