595bb7234dfa8efc3b82d0c64b01412098148830
Jrge <jorgemoymoya@gmail.com>
2024-07-26 22:01:05 -0400

    Modified footer component now is functional

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import Linkedin from "../assets/linkedin.svg";
 
 function Footer() {
   return (
-    <footer className="mt-60 flex flex-col bg-blue-500 text-white p-4 mt-8">
+    <footer className="mt-60 flex flex-col bg-blue-500 text-white p-4">
       <div className="container mx-auto flex flex-col items-center">
         <p className="text-center sm:text-left mb-4 sm:mb-0">
           © 2023 Jorge Moya.
