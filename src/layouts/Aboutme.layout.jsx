f0fbdcc1f352c3392677eacbd1bc3a5c7508d10b
Jrge <jorgemoymoya@gmail.com>
2024-07-27 23:40:33 -0400

    Create layout aboutme whit my most important information

---
+++ src/layouts/Aboutme.layout.jsx
@@ -0,0 +1,24 @@
+function AboutmeLayout() {
+  return (
+    <>
+      <h3 id="aboutme" className="font-bold text-2xl mt-20 m-10 text-center">
+        Sobre mí
+      </h3>
+      <div className="leading-relaxed text:sm sm:text-lg px-10 sm:px-64">
+        <div className="w-full h-full">
+          <p className="mb-">
+            Soy Analista Programador y estudiante de Ingeniería en Informática
+            con una sólida pasión por la resolución de problemas mediante la
+            programación. Busco aplicar mis habilidades y conocimientos
+            adquiridos en entornos prácticos, mientras continúo aprendiendo y
+            desarrollándome profesionalmente. Mi enfoque está en contribuir
+            eficazmente a proyectos innovadores y trabajar colaborativamente en
+            equipos multidisciplinarios.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AboutmeLayout;
