a270e275d800e978f97534a5c1aca9d9674ba5d7
Jrge <jorgemoymoya@gmail.com>
2024-07-27 21:26:41 -0400

    Modified Footer component, update the margin in icons and copyrigth

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,12 @@ import Linkedin from "../assets/linkedin.svg";
 
 function Footer() {
   return (
-    <footer className="mt-60 flex flex-col bg-blue-500 text-white p-4">
+    <footer className="mt-40 sm:mt-60 flex flex-col bg-blue-500 text-white p-4">
       <div className="container mx-auto flex flex-col items-center">
-        <p className="text-center sm:text-left mb-4 sm:mb-0">
-          © 2023 Jorge Moya.
+        <p className="text-center sm:text-left mb-4 ">
+          © 2024 Jorge Moya.
         </p>
-        <div className="flex space-x-4">
+        <div className="flex space-x-4 ">
           <a href="https://github.com/jl999l" className="hover:text-gray-400 transition duration-300">
             <img  src={Github} alt="GitHub" className="w-6 h-6 filter invert" />
           </a>
