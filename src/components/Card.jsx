bf8a0c4e992c943e2480fcb5ca2b2b2f9c4411f6
Jrge <jorgemoymoya@gmail.com>
2024-07-27 20:55:48 -0400

    Modified Card component for the dark mode

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,9 @@
-
-
 function Card(promp) {
   return (
     <>
-      <div className="w-72 h-52 rounded-lg border border-blue-500 p-5">
+      <div className="w-72 h-52 rounded-lg border dark:bg-slate-700 dark:text-white border-blue-500 p-5">
         <div className="flex space-x-4 text-xl justify-center">
-          <img className="w-5 h-5" src={promp.icon} alt="icon" />
+          <img className= "w-5 h-5 filter invert" src={promp.icon} alt="icon" />
           <p>{promp.title}</p>
         </div>
         <div className="text-sm mt-4">
