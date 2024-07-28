481ee46048f6dc964e7db8dc39742f32c277b29b
Jrge <jorgemoymoya@gmail.com>
2024-07-27 20:57:31 -0400

    Modified Home.view for the dark mode

--- src/views/Home.view.jsx
+++ src/views/Home.view.jsx
@@ -2,15 +2,16 @@ import Header from "../components/Header";
 import HomeLayout from "../layouts/Home.layout";
 import Footer from "../components/Footer";
 
-
 function HomeView() {
   return (
     <>
-      <Header />
-      <div className="w-auto h-auto">
-        <HomeLayout />
+      <div className="dark:bg-slate-900 text-white">
+        <Header />
+        <div className="w-auto h-auto">
+          <HomeLayout />
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </>
   );
 }
