5d2a1fc0780d1fcc9aca5b467fe1a379965ccfd4
Jrge <jorgemoymoya@gmail.com>
2025-05-09 23:26:52 -0400

    update: New disign presentation

--- src/layouts/Aboutme.layout.jsx
+++ src/layouts/Aboutme.layout.jsx
@@ -1,19 +1,14 @@
 function AboutmeLayout() {
   return (
     <>
-      <h3 id="aboutme" className="font-bold text-2xl mt-20 m-10 text-center">
-        Sobre mí
+      <h3 id="aboutme" className="text-2xl mt-20 m-10 text-center">
+        Sobre <b>Mí</b>
       </h3>
       <div className="leading-relaxed text:sm sm:text-lg px-10 sm:px-64">
         <div className="w-full h-full">
           <p className="mb-">
-            Soy Analista Programador y estudiante de Ingeniería en Informática
-            con una sólida pasión por la resolución de problemas mediante la
-            programación. Busco aplicar mis habilidades y conocimientos
-            adquiridos en entornos prácticos, mientras continúo aprendiendo y
-            desarrollándome profesionalmente. Mi enfoque está en contribuir
-            eficazmente a proyectos innovadores y trabajar colaborativamente en
-            equipos multidisciplinarios.
+            <b>Ingeniero en informática </b> y profe de tecnología en un liceo. Enseño programación, IA básica y electrónica.
+            Me gusta explorar distintas áreas del desarrollo y <b>aprender haciendo</b>
           </p>
         </div>
       </div>
