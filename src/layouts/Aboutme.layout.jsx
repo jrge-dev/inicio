a9f698ef80e3fe51752a0ef0ff94dac6587e480b
Jrge <jorgemoymoya@gmail.com>
2025-08-19 09:04:18 -0400

    update: restructure Aboutme and Home layouts for improved presentation

--- src/layouts/Aboutme.layout.jsx
+++ src/layouts/Aboutme.layout.jsx
@@ -1,18 +1,32 @@
+import Technologies from "../layouts/Technolgies.layout.jsx";
+
 function AboutmeLayout() {
   return (
-    <>
-      <h3 id="aboutme" className="text-2xl mt-20 m-10 text-center">
-        Sobre <b>Mí</b>
-      </h3>
-      <div className="leading-relaxed text:sm sm:text-lg px-10 sm:px-64">
-        <div className="w-full h-full">
-          <p className="mb-">
-            <b>Ingeniero en informática </b> y profe de tecnología en un liceo. Enseño programación, IA básica y electrónica.
-            Me gusta explorar distintas áreas del desarrollo y <b>aprender haciendo</b>
+    <div className="flex flex-row items-center justify-center w-full h-full">
+      <div className="w-1/2 flex flex-col items-center justify-center">
+        <div className="w-1/3">
+          <h3 id="aboutme" className="text-start text-4xl mt-20 mb-8 ">
+            Sobre <b>Mí</b>
+          </h3>
+          <p className="text-start text:sm sm:text-lg  ">
+            <b>Ingeniero en informática </b> y profe de tecnología en un liceo.
+            Enseño programación, IA básica y electrónica. Me gusta explorar
+            distintas áreas del desarrollo y <b>aprender haciendo</b>
           </p>
         </div>
       </div>
-    </>
+
+      <div className="w-1/2  flex-row justify-center items-center">
+         <h3
+          id="technologies"
+          className="font-bold text-2xl mt-20 m-10 text-center"
+        >
+          Tecnologías
+        </h3>
+
+        <Technologies></Technologies>
+      </div>
+    </div>
   );
 }
 
--- src/layouts/Home.layout.jsx
+++ src/layouts/Home.layout.jsx
@@ -5,6 +5,7 @@ import Frontend from "../assets/frontend.svg";
 import Banner from "../components/Banner";
 import Cpu from "../assets/cpu.svg";
 import AboutmeLayout from "./Aboutme.layout";
+import Technologies from "./Technolgies.layout";
 
 function Home() {
   const cardcontent = [
@@ -31,32 +32,32 @@ function Home() {
     <>
       <div className="flex flex-col">
         <div className="flex flex-col sm:flex-row justify-between items-center px-20 w-full max-w-screen">
+        <div className="mt-10 sm:mt-0 w-full h-full sm:px-20">
+            <img src={imageDeveloper} alt="image developer" />
+          </div>
           <div className="w-full h-full text-left text-xl sm:text-3xl">
-            <p className="text-blue-500 font-bold text-4xl sm:text-5xl mb-4 ">
-              ¡Hola me llamo!
+            <div className="text-center">
+            <p className=" text-2xl sm:text-4xl mb-2 ">
+              <span className="text-blue-500">¡Hola!</span> me llamo
             </p>
-            <p className="mb-2">
+            <p className="mb-2 text-7xl">
               <b>Jorge</b>
             </p>
-            <p className="mb-3 sm:mb-10">
-              Desarrollador de software <span> Curioso por naturaleza y con muchas ganas de seguir aprendiendo</span>
+            </div>
+            
+            <p className="text-4xl text-center mb-3 sm:mb-1">
+              Desarrollador de software 
             </p>
+            <p className="sm:text-lg mb-6"> Curioso por naturaleza y con muchas ganas de seguir aprendiendo</p>
 
             <Banner />
           </div>
-          <div className="mt-10 sm:mt-0 w-full h-full sm:px-20">
-            <img src={imageDeveloper} alt="image developer" />
-          </div>
+          
         </div>
 
         <AboutmeLayout />
 
-        <h3
-          id="technologies"
-          className="font-bold text-2xl mt-20 m-10 text-center"
-        >
-          Tecnologías
-        </h3>
+       
 
         <div className="flex  space-y-5 flex-col items-center sm:space-y-0 sm:flex-row sm:space-x-10 justify-center">
           {cardcontent.map((content) => (
